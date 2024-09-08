7f89f00d4ea930fbe5f1ad87b24c066deaa07554
Kiril-Lankov <k1r3t0@gmail.com>
2024-09-08 13:46:47 +0300

    favicon added in index.html

--- footbal-tournament/src/pages/Home.jsx
+++ footbal-tournament/src/pages/Home.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import HomePage from "../components/HomePage";
 
 const Home = () => {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
-};
+    return (
+        <div>
+            <HomePage/>
+        </div>
+    );
+}
 
-export default Home;
+export default Home
